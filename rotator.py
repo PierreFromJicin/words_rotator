3b920265c0a4b375d625648179c7f37a57a44bfd
Petr S <petr.strasik@gmail.com>
2022-10-16 17:38:57 +0200

    Version 1.0.2 (repair bug in work with end mark of sentence)

--- rotator.py
+++ rotator.py
@@ -4,24 +4,27 @@ def word_rotator_core(string_in=""):
     :param string_in:
     :return: string_out or False
     """
-    if type(string_in) is not str:                          # protection for a wrong input
+    if type(string_in) is not str:                                                  # protection for a wrong input
         return False
 
-    words_list = string_in.split(" ")                       # Prepare data
+    words_list = string_in.split(" ")                                               # Prepare data
     words_rev_list = []
+    _tmp = ""
 
-    for word in words_list:                                 # Letters parsing
+    for word in words_list:                                                         # Letters parsing
         word_l = list(word)
-        word_l.reverse()                                    # The core of rotation algorithm
+        word_l.reverse()                                                            # The core of rotation algorithm
         word_r = ""
         for _l in range(len(word_l)):
-            if word_l[_l] != ".":                           # Dot remove
+            if (word_l[_l] == ".") or (word_l[_l] == "!") or (word_l[_l] == "?"):   # Mark remove
+                _tmp = word_l[_l]
+            else:
                 word_r += word_l[_l]
-        words_rev_list.append(word_r.lower())               # Build final list
+        words_rev_list.append(word_r.lower())                                       # Build final list
 
     string_out = ""
     for word_rl in range(len(words_rev_list)):
-        string_out += words_rev_list[word_rl] + " "         # Building output string
+        string_out += words_rev_list[word_rl] + " "                                 # Building output string
 
-    string_out = (string_out[0: -1] + ".").capitalize()     # Prepare output string
+    string_out = (string_out[0: -1]).capitalize() + _tmp                            # Prepare output string
     return string_out
