d547f4bf0175bb6994261536e3178445789d5091
Petr S <petr.strasik@gmail.com>
2022-10-22 15:42:58 +0200

    Version 0.2.1 (added TODOs in main.py)

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from rotator import word_rotator_core as r_func
 
 
 print(r_func(input("Enter a sentence or a word: ")))
+
+# 1. Ošetřit zachytávání chyby v ve funkci word_rotator_core()
+# 2. Dodělat detektor velkých písmen
+# 3. Dodělat zadávání vět z CLI v cyklu (prompt: ri| )
+# 4. Dodělat přesměrování vstupu/výstupu z/do file.txt
