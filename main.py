9fdf437f49683e4fbc32136aed4db40a7c954615
Petr S <Petr.Strasik@gmail.com>
2022-10-22 17:09:11 +0200

    Version 0.2.1 (word_rotator_throwing_exception: retype TODOs in main.py)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from rotator import word_rotator_core as r_func
 
 print(r_func(input("Enter a sentence or a word: ")))
 
-# TODO 1. throwing exception in word_rotator_core() method
-# TODO 2. capital letter detector
-# TODO 3. cyclic input from the CLI (prompt: ri| )
-# TODO 4. redirect i/o from/to file.txt
+# TODO throwing exception in word_rotator_core() method
+# TODO capital letter detector
+# TODO cyclic input from the CLI (prompt: ri| )
+# TODO redirect i/o from/to file.txt
