75e0c707616357b0b6ce4da9f97a65de775fc016
Petr S <petr.strasik@gmail.com>
2022-10-23 11:25:23 +0200

    Version 0.2.1 (word_rotator_throwing_exception: created task list in tasks.md)

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ from rotator import word_rotator_core as r_func
 
 
 try:
-	print(r_func(False))
-	# print(r_func(input("Enter a sentence or a word: ")))
+	# print(r_func(False))
+	print(r_func(input("Enter a sentence or a word: ")))
 
 except rotator.WRCoreException as err:
 	print(err)
-
-# TODO 1. throwing exception in word_rotator_core() method
-# TODO 2. capital letter detector
-# TODO 3. cyclic input from the CLI (prompt: ri| )
-# TODO 4. redirect i/o from/to file.txt
---
+++ tasks.md
@@ -0,0 +1,5 @@
+# Tasks list:
+1. throwing exception in word_rotator_core() method ✔
+2. capital letter detector
+3. cyclic input from the CLI (prompt: ri| )
+4. redirect i/o from/to file.txt
