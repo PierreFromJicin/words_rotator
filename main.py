dc335593902782598ed06de6a18edee6b8678c69
Petr S <Petr.Strasik@gmail.com>
2022-11-06 22:00:46 +0100

    Version 0.2.3 (main: added cyclic input from the CLI)

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import rotator
 from rotator import word_rotator_core as r_func
 
-
 try:
 	# print(r_func(False))
-	print(r_func(input("Enter a sentence or a word: ")))
+	next_s: bool = False
+	str_in: str =''
+	while next_s is False:
+		print(r_func(input("Write your word or a sentence: ")))
+		str_in = input("To stop the rotator write 'stop', to continue press Enter.: ")
+		if str_in == "stop":
+			next_s = True
 
 except rotator.WRCoreException as err:
 	print(err)
