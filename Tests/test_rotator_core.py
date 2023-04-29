7f939c66437a37cdac5bee013337ac7630daa8ce
Petr S <Petr.Strasik@gmail.com>
2023-04-29 10:33:04 +0200

    reformat files

--- Tests/test_rotator_core.py
+++ Tests/test_rotator_core.py
@@ -4,9 +4,9 @@ from rotator import word_rotator_core
 
 
 def test_word_rotator_core_positive():
-	assert word_rotator_core("This is the testing sentence, with comma!") == "Siht si eht gnitset ecnetnes, htiw ammoc!"
+    assert word_rotator_core("This is the testing sentence, with comma!") == "Siht si eht gnitset ecnetnes, htiw ammoc!"
 
 
 def test_word_rotator_core_negative():
-	with pytest.raises(Exception):
-		assert word_rotator_core(False) == "Word rotator core exception: False is not a string"
+    with pytest.raises(Exception):
+        assert word_rotator_core(False) == "Word rotator core exception: False is not a string"
--- main.py
+++ main.py
@@ -2,27 +2,27 @@ import rotator
 from rotator import word_rotator_core as r_func
 
 try:
-	# print(r_func(False))
-	next_s: bool = True
-	str_in: str = ''
-	while next_s is True:
-		print(r_func(input("Write your word or a sentence: ")))
-		str_in = input("To stop the rotator write 'stop', "
-					   "to switch rotator into file write 'file', "
-					   "to continue press Enter.: ")
-		if str_in == "stop":
-			next_s = False
-		if str_in == "file":
-			data_results = []
-			with open("./input_files/input_file.txt", encoding='utf-8') as rotator_data:
-				for item in rotator_data.readlines():
-					line_string = str(item)
-					data_results.append(r_func(line_string) + '\n')
+    # print(r_func(False))
+    next_s: bool = True
+    str_in: str = ''
+    while next_s is True:
+        print(r_func(input("Write your word or a sentence: ")))
+        str_in = input("To stop the rotator write 'stop', "
+                       "to switch rotator into file write 'file', "
+                       "to continue press Enter.: ")
+        if str_in == "stop":
+            next_s = False
+        if str_in == "file":
+            data_results = []
+            with open("./input_files/input_file.txt", encoding='utf-8') as rotator_data:
+                for item in rotator_data.readlines():
+                    line_string = str(item)
+                    data_results.append(r_func(line_string) + '\n')
 
-			with open("./output_files/output_file.txt", 'w', encoding='utf-8') as output:
-				output.write(''.join([str(item) for item in data_results]))
-				print("The output_file.txt has been created.")
-	print("The game is over :)")
+            with open("./output_files/output_file.txt", 'w', encoding='utf-8') as output:
+                output.write(''.join([str(item) for item in data_results]))
+                print("The output_file.txt has been created.")
+    print("The game is over :)")
 
 except rotator.WRCoreException as err:
-	print(err)
+    print(err)
